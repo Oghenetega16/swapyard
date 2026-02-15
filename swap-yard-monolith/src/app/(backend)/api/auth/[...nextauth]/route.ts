81414136787e05ed0784c35b340ebae9fe6f29b7
westwardluxe <westwardluxegroup.co@gmail.com>
2026-02-15 12:36:26 +0100

    Updated schema to allow for Google direct login

--- swap-yard-monolith/src/app/(backend)/api/auth/[...nextauth]/route.ts
+++ swap-yard-monolith/src/app/(backend)/api/auth/[...nextauth]/route.ts
@@ -1,49 +1,15 @@
-import NextAuth from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-import { prisma } from "@/lib/prisma";
-import bcrypt from "bcryptjs";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
 
-const handler = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [  
-    Credentials({
-        name: "Credentials",
-        credentials: {
-          email: { label: "Email", type: "email", placeholder: "Enter your email" },
-          password: { label: "Password", type: "password", placeholder: "Enter your password" },
-
-        },
-        async authorize(credentials) {
-          if (!credentials?.email || !credentials?.password) {
-            return null;
-          }
-
-          const user = await prisma.user.findUnique({
-            where: { email: credentials.email }
-          });
-
-          if (!user || !user.password) {
-            return null;
-          }
-
-          const isPasswordValid = await bcrypt.compare(credentials.password, user.password);
-
-          if (!isPasswordValid) {
-            return null;
-          }
-
-          return {
-            id: user.id,
-            email: user.email,
-            name: user.name
-          };
-        }
-      })
+export const authOptions = {
+ 
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
   ],
-  pages: {
-    signIn: "/login",
-  },
-});
+}
 
-export { handler as GET, handler as POST };
+export default NextAuth(authOptions)
