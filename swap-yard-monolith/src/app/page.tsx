53e329b7ccdf625e8feebe00329926e1a7245628
oghenetega16 <sukuruoghenetega@gmail.com>
2026-02-20 06:59:41 +0100

    chore: update monolith dependencies and adjust page setup

--- swap-yard-monolith/src/app/page.tsx
+++ swap-yard-monolith/src/app/page.tsx
@@ -3,7 +3,7 @@ import Page from "./(frontend)/landingpage/page";
 export default function Home() {
     return (
         <main className="">
-           
+
             <Page />
         </main>
     );
