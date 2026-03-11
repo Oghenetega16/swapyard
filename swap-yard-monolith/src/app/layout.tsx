21442b7509653a4a51d147d167afa138b1260152
Arthur Onyeanusi <arthuronyeanusi@gmail.com>
2026-03-11 16:10:16 +0100

    Switched to experimental https

--- swap-yard-monolith/src/app/layout.tsx
+++ swap-yard-monolith/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import FacebookProvider from "./auth/components/facebooksdk";
 import "./globals.css";
 // Import your new wrapper here
 import { ClientNavigation } from "@/components/layouts/ClientNavigation"; 
@@ -24,7 +25,8 @@ export default function RootLayout({
         <html lang="en">
             <body className={`${manrope.className} min-h-screen flex flex-col bg-[#F9FAFB]`} suppressHydrationWarning>
                 <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_ID!}>
-                    
+                
+                    <FacebookProvider/>
                     <ClientNavigation />
                     
                     {/* Main content area */}
