2880a0d91229922507852f0796975f33ff3b2be4
Arthur Onyeanusi <arthuronyeanusi@gmail.com>
2026-03-12 07:45:42 +0100

    Added Put method for me route and included validation to prevent malicious or accidental entries

--- swap-yard-monolith/src/app/(backend)/api/auth/me/route.ts
+++ swap-yard-monolith/src/app/(backend)/api/auth/me/route.ts
@@ -2,6 +2,7 @@ import { cookies } from "next/headers";
 import { verifyToken } from "@/lib/token";
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import { updateProfileSchema } from "../schema";
 
 export async function GET() {
   try {
@@ -40,5 +41,65 @@ export async function GET() {
   } catch (error) {
     console.error("Error fetching user data:", error);
     return NextResponse.json({ message: "Internal server error" }, { status: 500 });
+  }
+}
+
+export async function PUT(req: Request) {
+  try {
+    const token = (await cookies()).get("session")?.value;
+
+    if (!token) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const payload = await verifyToken(token);
+    const userId = typeof payload === "string" ? payload : payload?.userId;
+
+    if (!userId) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const body = await req.json();
+
+    const parsed = updateProfileSchema.safeParse(body);
+
+    if (!parsed.success) {
+      return NextResponse.json(
+        { errors: parsed.error.flatten().fieldErrors },
+        { status: 400 }
+      );
+    }
+
+    const data = parsed.data;
+
+    const updatedUser = await prisma.user.update({
+      where: { id: userId },
+      data,
+      select: {
+        id: true,
+        email: true,
+        firstname: true,
+        lastname: true,
+        phoneNumber: true,
+        role: true,
+        state: true,
+        contract: true,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        message: "Profile updated successfully",
+        user: updatedUser,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error updating user:", error);
+
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
   }
 }
